bb7c4ac980077b672a77c955669e2b315acb1c7a
YwaGenemy <a.a.kolodeznikov@gmail.com>
2025-08-09 14:59:27 +0900

    update html -> head

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <!doctype html>
+<html>
+    <head>
+        <link
+            rel = "stylesheet"
+            href = "style.css"
+            >
+        <title> Chess </title>
+    </head>
+
+    <body>
+        <div>
+            Part
+        </div>
+    </body>
+</html>
